e7d28fddb52e7333ef3cee6c041c4d7309c0159c
jodfedlet <fedletpierre15@gmail.com>
2024-02-12 13:17:15 -0300

    chore: openapi configurations

--- src/main/java/me/jodfedlet/thewine/TheWineApplication.java
+++ src/main/java/me/jodfedlet/thewine/TheWineApplication.java
@@ -1,9 +1,11 @@
 package me.jodfedlet.thewine;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @io.swagger.v3.oas.annotations.info.Info(title = "The Wine API", version = "1.0", description = "The Wine API"))
 public class TheWineApplication {
 
 	public static void main(String[] args) {
--- src/main/java/me/jodfedlet/thewine/modules/customer/CustomerController.java
+++ src/main/java/me/jodfedlet/thewine/modules/customer/CustomerController.java
@@ -1,6 +1,10 @@
 package me.jodfedlet.thewine.modules.customer;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import me.jodfedlet.thewine.modules.customer.dto.in.CreateCustomerInDto;
 import me.jodfedlet.thewine.modules.customer.dto.out.CustomerOutDto;
 import me.jodfedlet.thewine.modules.customer.service.CustomerService;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "customers", description = "The customer implementation")
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
@@ -18,26 +23,36 @@ public class CustomerController {
     private CustomerService customerService;
 
     @PostMapping
+    @Operation(summary = "Create a new customer")
+    @ApiResponse(responseCode = "201", description = "Customer created")
     public ResponseEntity<CustomerOutDto> createCustomer(@RequestBody CreateCustomerInDto createCustomerInDto) {
         return ResponseEntity.ok(customerService.create(createCustomerInDto));
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find a customer by id")
+    @ApiResponse(responseCode = "200", description = "Customer found")
     public ResponseEntity<CustomerOutDto> findOne(@PathVariable String id) {
         return ResponseEntity.ok(customerService.findById(id));
     }
 
     @GetMapping
+    @Operation(summary = "Find all customers")
+    @ApiResponse(responseCode = "200", description = "Customers found")
     public ResponseEntity<List<CustomerOutDto>> findAll() {
         return ResponseEntity.ok(customerService.findAll());
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update a customer")
+    @ApiResponse(responseCode = "200", description = "Customer updated")
     public ResponseEntity<CustomerOutDto> update(@PathVariable String id, @RequestBody CreateCustomerInDto createCustomerInDto) {
         return ResponseEntity.ok(customerService.update(id, createCustomerInDto));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a customer")
+    @ApiResponse(responseCode = "204", description = "Customer deleted")
     public ResponseEntity<Void> delete(@PathVariable String id) {
         customerService.delete(id);
         return ResponseEntity.noContent().build();
